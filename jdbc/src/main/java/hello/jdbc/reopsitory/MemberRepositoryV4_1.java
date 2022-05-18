0b4df6254a04e70c0d540c679f325d2b91a6bf2e
ats3059 <ats3059@naver.com>
2022-05-18 22:46:51 +0900

    spring execption study

--- jdbc/src/main/java/hello/jdbc/reopsitory/MemberRepositoryV4_1.java
+++ jdbc/src/main/java/hello/jdbc/reopsitory/MemberRepositoryV4_1.java
@@ -1,6 +1,7 @@
 package hello.jdbc.reopsitory;
 
 import hello.jdbc.domain.Member;
+import hello.jdbc.reopsitory.ex.MyDbException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jdbc.support.JdbcUtils;
@@ -10,36 +11,36 @@ import java.sql.*;
 import java.util.NoSuchElementException;
 
 /**
- *  트랜잭션 - 트랜잭션 매니저
- *  DataSourceUtils.getConnection()
- *  DataSourceUtils.relaeseConnection()
+ *  예외 누수 문제 해결
+ *  체크 예외를 런타임 예외로 변경
+ *  MemberRepository 인터페이스 사용
+ *  throws SQLException 제거
  */
 @Slf4j
-public class MemberRepositoryV3{
+public class MemberRepositoryV4_1 implements MemberRepository{
 
     private final DataSource dataSource;
 
-    public MemberRepositoryV3(DataSource dataSource) {
+    public MemberRepositoryV4_1(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    public Member save(Member member) throws SQLException {
+    @Override
+    public Member save(Member member){
         String sql = "insert into member(member_id,money) values (?,?)";
 
         Connection con = null;
         PreparedStatement pstmt = null;
 
-
-        con = getConnection();
         try {
+            con = getConnection();
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1,member.getMemberId());
             pstmt.setInt(2,member.getMoney());
             pstmt.executeUpdate();
             return member;
         } catch (SQLException e) {
-            log.error("db error" , e);
-            throw e;
+            throw new MyDbException(e);
         }finally {
             close(con,pstmt,null);
         }
@@ -47,7 +48,8 @@ public class MemberRepositoryV3{
 
     }
 
-    public Member findById(String memberId) throws SQLException {
+    @Override
+    public Member findById(String memberId) {
         String sql = "select * from member where member_id = ?";
 
         Connection con = null;
@@ -69,8 +71,7 @@ public class MemberRepositoryV3{
             }
 
         } catch (SQLException e) {
-            log.error("db error",e);
-            throw e;
+            throw new MyDbException(e);
         }finally {
             close(con,pstmt,rs);
         }
@@ -78,8 +79,8 @@ public class MemberRepositoryV3{
 
     }
 
-
-    public void update(String memberId , int money) throws SQLException {
+    @Override
+    public void update(String memberId , int money){
         String sql = "update member set money=? where member_id=?";
 
         Connection con = null;
@@ -93,15 +94,14 @@ public class MemberRepositoryV3{
             int resultSize = pstmt.executeUpdate();
             log.info("resultSize = {} " , resultSize);
         } catch (SQLException e) {
-            log.error("db error" , e);
-            throw e;
+            throw new MyDbException(e);
         }finally {
             close(con,pstmt,null);
         }
     }
 
-
-    public void delete(String memberId ) throws SQLException {
+    @Override
+    public void delete(String memberId ){
         String sql = "delete from member where member_id = ?";
 
         Connection con = null;
@@ -113,8 +113,7 @@ public class MemberRepositoryV3{
             pstmt.setString(1,memberId);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            log.error("db error" , e);
-            throw e;
+            throw new MyDbException(e);
         }finally {
             close(con,pstmt,null);
         }
---
+++ jdbc/src/main/java/hello/jdbc/reopsitory/ex/MyDuplicateKeyException.java
@@ -0,0 +1,20 @@
+package hello.jdbc.reopsitory.ex;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+public class MyDuplicateKeyException extends MyDbException {
+    public MyDuplicateKeyException() {
+    }
+
+    public MyDuplicateKeyException(String message) {
+        super(message);
+    }
+
+    public MyDuplicateKeyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyDuplicateKeyException(Throwable cause) {
+        super(cause);
+    }
+}
